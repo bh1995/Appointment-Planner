e0da3d4da8e1858758db7086c24d2149c7327233
Bjørn Hansen <55191733+bh1995@users.noreply.github.com>
2023-11-06 14:12:53 +0000

    broken

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Tile } from "../tile/Tile";
 
-export const TileList = ({ items = [] }) => {
+export const TileList = ( {items} ) => {
   if (!Array.isArray(items)) {
     // This will catch null, undefined, or any non-array
     console.error("items prop is not an array", items);
@@ -9,8 +9,14 @@ export const TileList = ({ items = [] }) => {
   }
   return (
     <div>
-      {items.map((item, index) => (
-        <Tile key={index} name={item.name} description={item.description} />
+      {items.map((item) => (
+        <div className="tile-container">
+        <p className="tile-name">{item.name}</p>
+        {/* Now map over the other properties and render them */}
+        {Object.keys(otherProps).map((key, index) => (
+          <p key={index}>{`${key}: ${otherProps[key]}`}</p>
+        ))}
+      </div>
       ))}
     </div>
   );
